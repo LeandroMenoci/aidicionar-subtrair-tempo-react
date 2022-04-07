d9f696c9eda264e97a63fbfefbdd4a6bcbbdaad4
LeandroMenoci <menoci.leandro@gmail.com>
2022-04-07 15:41:09 -0300

    Aula completa de como fazer um contador de datas

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parser: '@babel/eslint-parser',
+  requireConfigFile: false,
+
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'react-hooks',
+  ],
+  rules: {
+    'react/jsx-filename-extension': 0,
+  },
+};
---
+++ README.old.md
@@ -0,0 +1,2 @@
+# aidicionar-subtrair-tempo-react
+Adicionar ou subtrair tempo em datas com React + JS
